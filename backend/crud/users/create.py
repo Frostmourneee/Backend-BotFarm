9c3f5612e630a0f3d5839986ae804a6d3062714f
frostmourneee <wellthx1@gmail.com>
2025-11-23 12:25:53 +0300

    api/hashing added

--- backend/crud/users/create.py
+++ backend/crud/users/create.py
@@ -2,11 +2,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.db.models.user import User
 from backend.api.schemas.users.create import UserCreate
+from backend.db.utils import get_password_hash
 
 async def create_user(session: AsyncSession, user_data: UserCreate) -> None:
     """
     Регистрирует нового пользователя в системе.
     """
-    user = User(**user_data.model_dump(exclude_unset=True))
+    hashed_password = get_password_hash(user_data.password)
+    user = User(
+        **user_data.model_dump(exclude_unset=True, exclude={"password"}),
+        password=hashed_password
+    )
+
     session.add(user)
     await session.commit()
